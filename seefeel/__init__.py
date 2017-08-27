4782d0d89f441b67a69604440002272f2f52f4c1
杨飞 <yangfei@hudongpai.com>
2017-08-27 17:08:37 +0800

    install

--- seefeel/__init__.py
+++ seefeel/__init__.py
@@ -1,6 +1,9 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 # author: seefeel
-# date: 2017-08-24
+# date: 2017-08-27
 
-import preprocess
+import clean
+import clusterutils
+import stringutils
+import tokenizer
--- seefeel/preprocess/__init__.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-# -*- coding: utf-8 -*-
-# author: seefeel
-# date: 2017-08-27
-
-import clean
-import clusterutils
-import stringutils
-import tokenizer
